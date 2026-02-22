9f9be515fd63898503f2a6442341da9e96038626
harms <yao.hgk@obmontessori.edu.ph>
2026-02-23 06:34:51 +0800

    Update player.py

--- player.py
+++ player.py
@@ -4,6 +4,9 @@ def display_players(e):
     document.getElementById('output').innerHTML = " "
 
     players = ['Abdullah','Abeleda','Arce','Arias','Bonzon','Cajucom','Catimbang', 'Choi', 'Cotioco','Daradal','Enriquez','Escobar', 'Espina', 'Gano','Garcia','Kaur','Ong','Rufo','Sanchez','Santos','Tan','Vilale','Yao','Zosa']
+    count = 1
 
     for p in players: 
-        display(f"-{p}", target="output")
+        display(f"{count}) {p}", target="output")
+        count += 1
+
